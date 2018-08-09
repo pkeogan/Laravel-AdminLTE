d8408f0f1267570869b79ac3d65b5d50435f17a6
Peter <peter.keogan@hcmed.org>
2018-08-09 15:51:13 +0000

    update to acttionbuttons

--- src/views/actionbuttons.blade.php
+++ src/views/actionbuttons.blade.php
@@ -16,29 +16,37 @@
     |
     */
 
+
+
+
 @endphp
 
-<div class="hidden-xs">
+<div class="hidden action-buttons-large" data-buttons-count="{{ collect($buttons)->whereNotIn('seperator', 'seperator')->count() }}">
   <div class="btn-group">
     @foreach($buttons as $button)
+	  @if(!isset($button['seperator']))
         @can($button['permission'] )
-	      <a data-toggle="tooltip" title="{{$button['label']}}" class="btn {{$button['style']}}" href="{{$button['link']}}"> <i class="{{$button['icon']}}"></i> </a>
+	  	      	<a data-toggle="tooltip" data-do-not-animate title="{{$button['label']}}" class="btn {{$button['style']}}" @if(isset($button['data-confirmation']))data-confirm-link="{{$button['link']}}"@else href="{{$button['link']}}" @endif> <i class="{{$button['icon']}}"></i> </a>
         @endcan
+	   @endif
     @endforeach
   </div>
 </div>
-<div class="visible-xs-block">
-  <div class="btn-group">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+<div class="action-buttons-small">
+  <div class="btn-group btn-block">
+    <button type="button" class="btn btn-default btn-block dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
   @if(isset($submenuTitle)){{ $submenuTitle }}@else Tools @endif  <span class="caret"></span>
   <span class="sr-only">Toggle Dropdown</span>
   </button>
     <ul class="dropdown-menu" role="menu">
       @foreach($buttons as $button)
-        @haspermission($button['permission'] )
-              <li>
-                <a href="{{$button['link']}}">{{$button['label']}}</a></li>
-        @endhaspermission
+		@if(isset($button['seperator']))
+			  			<li role="separator" class="divider"></li>
+	  		@else
+        @can($button['permission'] )
+              <li><a data-do-not-animate  @if(isset($button['data-confirmation']))data-confirm-link="{{$button['link']}}"@else href="{{$button['link']}}" @endif >{{$button['label']}}</a></li>
+        @endcan
+		@endif
       @endforeach
     </ul>
   </div>
